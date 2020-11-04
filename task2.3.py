765179f167041bfd20e23919a693cfbb863ed8b6
OlehYavoriv <73217391+OlehYavoriv@users.noreply.github.com>
2020-11-04 17:07:34 +0200

    Create task2.3.py

---
+++ task2.3.py
@@ -0,0 +1,7 @@
+print("ax^2+bx+c")
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+c = float(input("Enter third number:"))
+x1 = (((-b) + (b**2-4*a*c))/2*a)
+x2 = (((-b) - (b**2-4*a*c))/2*a)
+print("Result:", x1 ,";", x2)
