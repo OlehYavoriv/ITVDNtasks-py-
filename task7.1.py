509b83ba953846650fe1900a4777df01e31a94e1
OlehYavoriv <73217391+OlehYavoriv@users.noreply.github.com>
2020-11-04 16:30:43 +0200

    Create task7.1.py

---
+++ task7.1.py
@@ -0,0 +1,7 @@
+lists = [5, 45, 12, 5, 3, 1, 120]
+print("Max element: ", max(lists))
+print("Min element :", min(lists))
+sum = max(lists) + min(lists)
+print(sum)
+diya = (max(lists) + min(lists)) / 2
+print(diya)
