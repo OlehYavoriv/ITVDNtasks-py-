8f915e4bb0f3e232fcf60fac556cf31e70bde701
OlehYavoriv <73217391+OlehYavoriv@users.noreply.github.com>
2020-11-04 16:46:04 +0200

    Create task2.2.py

---
+++ task2.2.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first integer number :"))
+b = int(input("Enter second  integer number :"))
+x = int(input("Enter third  integer number :"))
+
+if x>=a and x<=b :
+    print("True")
+else:
+    print("False")
